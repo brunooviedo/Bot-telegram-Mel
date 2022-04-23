d8921d71404a849c91d96cbf2afcf2ab13757b56
Bruno <bruno.oviedo89@gmail.com>
2022-04-23 00:28:57 -0400

    Primer commit

---
+++ app.py
@@ -0,0 +1,83 @@
+from unittest import result
+from bs4 import BeautifulSoup
+import requests
+import schedule
+import time
+import urllib.request
+import pandas as pd
+
+
+def bot_send_text(bot_message):
+    
+    bot_token = '5231406261:AAE1lr7A9feeiv9Ejt3awEyigwzpxtyoqRo'
+    bot_chatID = '-796627951'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+
+
+
+
+def sismo_scraping():
+    e = urllib.request.urlopen("http://www.sismologia.cl/ultimos_sismos.html").read()
+    soup = BeautifulSoup(e, 'html.parser')
+
+    # Obt    enemos la tabla
+
+    tabla_sismos = soup.find_all('table')[0]
+
+    # Obtenemos todas las filas
+    rows = tabla_sismos.find_all("tr")
+
+    output_rows = []
+    for row in rows:
+        # obtenemos todas las columns
+        cells = row.find_all("td")
+        output_row = []
+        if len(cells) > 0:
+            for cell in cells:
+                output_row.append(cell.text)
+            output_rows.append(output_row)
+
+    dataset = pd.DataFrame(output_rows)
+
+    dataset.columns = [
+        "Fecha Local",
+        "Fecha UTC",
+        "Latitud",
+        "Longitud",
+        "Profundidad [Km]",
+        "Magnitud",
+        "Referencia Geográfica",
+    ]
+    dataset[["Latitud", "Longitud"]] = dataset[["Latitud", "Longitud"]].apply(pd.to_numeric)
+    
+    dataset_filter = dataset[
+            (-27.100 <= dataset["Latitud"])
+            & (dataset["Latitud"] <= -21.680)
+            & (-72.150 <= dataset["Longitud"])
+            & (dataset["Longitud"] <= -66.180)
+            ]
+    
+    string="A ocurrido un sismo en las cercanías de Minera Escondida" "\n"
+    for column in dataset_filter.head(1).columns:
+        string += column + " : " + str(dataset_filter[column].values[0]) + "\n"
+    return string
+   
+        
+def main():
+    ultimo_sismo = (None)
+    while True:
+        text = f'{sismo_scraping()}'
+        if (text) != ultimo_sismo:
+            bot_send_text(text)
+            ultimo_sismo = (text)
+        time.sleep(60) 
+
+
+if __name__ == '__main__':
+        
+
+    main()
